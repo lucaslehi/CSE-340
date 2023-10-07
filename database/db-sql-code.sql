1ebd76694b975e770f2daf9b6451be59dd661c43
Lucas Gomes <lucaslehi@byui.edu>
2023-10-06 19:05:52 -0600

    updates

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -3,7 +3,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340la;
+    OWNER TO cse340lg;
 
 -- Table structure for table `classification`
 CREATE TABLE public.classification (
